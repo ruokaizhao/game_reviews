78d2744e5a4f1aa6e0288c72c00d836247a8e494
Ruokai Zhao <lelouch_l@icloud.com>
2022-02-18 04:51:20 -0500

    Creates handleSubmit function to handle new review

--- react-frontend/src/components/NewReview.js
+++ react-frontend/src/components/NewReview.js
@@ -3,7 +3,22 @@ import React, { useState } from "react";
 function NewReview({ game_id, testUser, onAddNewReview }) {
   const [newReview, setNewReview] = useState("")
 
-
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch("http://localhost:9292/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user_id: testUser.user_id,
+        game_id: game_id,
+        review: newReview
+      })
+    })
+    .then((r) => r.json())
+    .then((updatedReview) => onAddNewReview(updatedReview))
+  }
 
 
   return (
