4ec1fa11aa8b1ec4eb9bd3686a2aef67ffccf073
Ruokai Zhao <lelouch_l@icloud.com>
2022-02-17 23:08:04 -0500

    Creates Review.js and adds some routing

--- react-frontend/src/components/Game.js
+++ react-frontend/src/components/Game.js
@@ -1,30 +1,28 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Route, NavLink, useRouteMatch, useParams } from "react-router-dom";
+import Review from "./Review"
 
 function Game(props) {
-  const[showReview, setShowReview] = useState(false)
 
   const {name, price, company, introduction, img} = props.game
+  const match = useRouteMatch()
 
   return (
     <div>
-      <div className="container">
-        <h2>{name}</h2>
-        <NavLink to="/"
-        <img className="image" src={img} alt={name}/>
-        <h3>From: {company}</h3>
-        <h4>Price: {price}</h4>
-      </div>
-      {
-        showReview ?
-        <Review />
-        : null
-      }
-
-    </div>
-    
+      <ul>
+        <li>
+          <NavLink to={`/games/${name}`}>
+            <h2>{name}</h2>
+          </NavLink>        
+          <h3>From: {company}</h3>
+          <h4>Price: {price}</h4>
+          <Route path={`/games/:gameId`}>
+            <Review name={name} introduction={introduction} img={img}/>
+          </Route>
+        </li>
+      </ul>      
+    </div>    
   )
-
 }
 
 
