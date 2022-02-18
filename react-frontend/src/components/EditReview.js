8ec66dfe00a4896f5777ee780c5079a0eece6517
Ruokai Zhao <lelouch_l@icloud.com>
2022-02-18 07:07:46 -0500

    Finishes EditReview component

--- react-frontend/src/components/EditReview.js
+++ react-frontend/src/components/EditReview.js
@@ -4,7 +4,20 @@ import React, { useState } from "react";
 function EditReview({ review, id, onEditReview }) {
   const [editedReview, setEditedReview] = useState(review)
 
-
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch(`http://localhost:9292/reviews/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        review: editedReview
+      })
+    })
+    .then((r) => r.json())
+    .then((updatedReview) => onEditReview(updatedReview))
+  }
 
   return (
     <div>
